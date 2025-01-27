714d04973629ad6cbebc7dd7d0b0353a1037e088
A3888S <sowdirect@gmail.com>
2025-01-27 09:26:05 +0000

    Оновлено репозиторій

---
+++ python_kazah/sql_ptoject/order_insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO Shop_order(user_id, status, address, mobile)
+VALUES
+    ("1", "new", "Ahunbaeva 95", "0999134494"),
+    ("2", "confirmed", "Baitik Vatyra 7A", "0777 666 444"),
+    ("4", "new", "Київська 78/2", "0555333222");
